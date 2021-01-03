a62977345faf2c2196eeb69dfb2e72f053e589dd
YoussefShamss <youssef1shams@gmail.com>
2021-01-03 17:38:37 +0200

    Added Trap drawing and implemented in DrawGrid

--- main.asm
+++ main.asm
@@ -2397,6 +2397,93 @@ DrawBigDot macro xPosition, yPosition, fillColor, backgroundColor
 endm DrawBigDot
 
 ;----------------------------------------------------------------------------------------
+; Trap
+;----------------------------------------------------------------------------------------
+DrawTrap macro xPosition,yPosition,backGroundColor,bodyColor1,bodyColor2
+mov cx,xPosition
+mov dx,yPosition
+;row1
+DrawHorizontalLine backGroundColor,3
+DrawHorizontalLine bodyColor1,4
+DrawHorizontalLine backGroundColor,3
+;row2
+mov cx,xPosition
+inc dx
+DrawHorizontalLine backGroundColor,2
+DrawHorizontalLine bodyColor2,1
+DrawHorizontalLine bodyColor2,1
+DrawHorizontalLine bodyColor1,2
+DrawHorizontalLine bodyColor2,1
+DrawHorizontalLine bodyColor2,1
+DrawHorizontalLine backGroundColor,2
+;row3
+mov cx,xPosition
+inc dx
+DrawHorizontalLine backGroundColor,1
+DrawHorizontalLine bodyColor1,1
+DrawHorizontalLine bodyColor2,1
+DrawHorizontalLine 14,1
+DrawHorizontalLine bodyColor2,2
+DrawHorizontalLine 14,1
+DrawHorizontalLine bodyColor2,1
+DrawHorizontalLine bodyColor1,1
+DrawHorizontalLine backGroundColor,1
+
+;row4
+mov cx,xPosition
+inc dx
+DrawHorizontalLine backGroundColor,1
+DrawHorizontalLine bodyColor1,8
+DrawHorizontalLine backGroundColor,1
+
+;row5
+mov cx,xPosition
+inc dx
+DrawHorizontalLine bodyColor1,2
+DrawHorizontalLine 0fh,6
+DrawHorizontalLine bodyColor1,2
+
+;row6
+mov cx,xPosition
+inc dx
+DrawHorizontalLine bodyColor1,2
+DrawHorizontalLine 4,6
+DrawHorizontalLine bodyColor1,2
+
+;row7
+mov cx,xPosition
+inc dx
+DrawHorizontalLine bodyColor1,3
+DrawHorizontalLine 0ch,4
+DrawHorizontalLine bodyColor1,3
+
+;row8
+mov cx,xPosition
+inc dx
+DrawHorizontalLine bodyColor1,2
+DrawHorizontalLine bodyColor2,1
+DrawHorizontalLine bodyColor1,4
+DrawHorizontalLine bodyColor2,1
+DrawHorizontalLine bodyColor1,2
+
+;row9
+mov cx,xPosition
+inc dx
+DrawHorizontalLine backGroundColor,1
+DrawHorizontalLine bodyColor1,2
+DrawHorizontalLine bodyColor2,4
+DrawHorizontalLine bodyColor1,2
+DrawHorizontalLine backGroundColor,1
+
+;row10
+mov cx,xPosition
+inc dx
+DrawHorizontalLine backGroundColor,2
+DrawHorizontalLine bodyColor1,6
+DrawHorizontalLine backGroundColor,2
+
+endm DrawTrap
+;----------------------------------------------------------------------------------------
 
 GridToCell macro gridX, gridY ; takes xPosition, yPosition, puts the cell number in bx
 	mov bx, word ptr gridY
@@ -2514,7 +2601,7 @@ endm FindPath
 	cherryCode          equ 19
 	dotCode             equ 20
 	bigDotCode          equ 21
-	greenDotCode        equ 22
+	trapCode            equ 22
 	extraLifeCode       equ 23
 	decLifeCode         equ 24
 	cornerLeftUpCode    equ 25
@@ -2774,7 +2861,7 @@ MovePacman proc
 	                       je                      ApplyCherry1
 	                       cmp                     grid[bx], bigDotCode
 	; je                     ApplyBigDot1
-	                       cmp                     grid[bx], greenDotCode
+	                       cmp                     grid[bx], trapCode
 	; je                     ApplyGreenDot1
 	                       cmp                     grid[bx], extraLifeCode
 	                       je                      ApplyPacmanLife1
@@ -2860,7 +2947,7 @@ MovePacman proc
 	                       je                      ApplyCherry2
 	                       cmp                     grid[bx], bigDotCode
 	; je                     ApplyBigDot2
-	                       cmp                     grid[bx], greenDotCode
+	                       cmp                     grid[bx], trapCode
 	; je                     ApplyGreenDot2
 	                       cmp                     grid[bx], extraLifeCode
 	                       je                      ApplyPacmanLife2
@@ -2962,6 +3049,8 @@ DrawGrid proc
 	                       je                      Dot
 	                       cmp                     grid[si], bigDotCode
 	                       je                      BigDot
+	                       cmp                     grid[si], trapCode
+	                       je                      Trap
 	                       cmp                     grid[si], cornerLeftDownCode
 	                       je                      CornerLeftDown
 	                       cmp                     grid[si], cornerLeftUpCode
@@ -3047,6 +3136,9 @@ DrawGrid proc
 	                       jmp                     ContinueDraw
 	BigDot:                
 	                       DrawBigDot              currentX, currentY, white, backgroundColor
+	                       jmp                     ContinueDraw
+	Trap:                  
+	                       DrawTrap                currentX, currentY, backGroundColor, lightGreen, green
 	                       jmp                     ContinueDraw
 	CornerLeftUp:          
 	                       DrawCornerWallLeftUp    currentX, currentY, black, white, backgroundColor
@@ -3195,7 +3287,7 @@ main proc far
 	LoadingMenu:           
 	                       SetVideoMode
 
-	DrawLoadingScreen       black,yellow,cyan                                                            	;The next code snippet is ofr the delay
+	                       DrawLoadingScreen       black,yellow,cyan                                                            	;The next code snippet is ofr the delay
 	                       MOV                     CX, 3fH
 	                       MOV                     DX, 4240H
 	                       MOV                     AH, 86H
@@ -3321,6 +3413,12 @@ main proc far
 	                       mov                     grid[389], verticalWallCode
 	                       mov                     grid[419], verticalWallCode
 	                       mov                     grid[449], verticalWallCode
+	; Testing item drawing functions.
+	                       mov                     grid[142], trapCode
+	                       mov                     grid[87], cherryCode
+	                       mov                     grid[288], snowflakeCode
+	                       mov                     grid[266], snowflakeCode
+	                       mov                     grid[400], cherryCode
 						   
 
 	                       mov                     si, @data
